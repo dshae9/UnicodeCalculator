2494284d5be37677b237e1618d8b9bf3662b2713
Demonshae Glover <dshaeglover@gmail.com>
2019-06-06 05:12:46 -0400

    first upload to git

---
+++ src/Unicode.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Unicode {
+public static void main(String[] args) {
+	
+	String string word = "Grand Circus";
+	
+	
+	for (int i = 0; i < word.length(); i++)
+		System.out.print(word.charAt(i));
+	char ch = word;
+	int i = ch;
+	System.out.println("The value of " + ch + " = " + i);
+	
+
+
+}
+}
