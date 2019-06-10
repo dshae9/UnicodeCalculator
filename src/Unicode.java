bbc7944eb53fb7de5d7e2ad24dbf5cf6a1646d74
Demonshae Glover <dshaeglover@gmail.com>
2019-06-10 07:15:18 -0400

    updated code so that project compiles

--- src/Unicode.java
+++ src/Unicode.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
 
+
 public class Unicode {
 public static void main(String[] args) {
 	
-	String string word = "Grand Circus";
+	Scanner scnr = new Scanner(System.in);
+	System.out.print("Enter first string here: ");
+	String s1 = scnr.nextLine();
+	System.out.print("Enter second string here: ");
+	String s2 = scnr.nextLine();
+	System.out.println();
 	
-	
-	for (int i = 0; i < word.length(); i++)
-		System.out.print(word.charAt(i));
-	char ch = word;
-	int i = ch;
-	System.out.println("The value of " + ch + " = " + i);
-	
-
+	int sum1 = 0 , sum2 = 0, c;
+	for(int i = 0; i < s1.length(); i++) {
+		c = (int)s1.charAt(i);
+		System.out.println(s1.charAt(i) + " : " + c);
+		sum1 += c;
+	}
+	System.out.println();
+	for(int i = 0; i < s2.length(); i++) {
+		c = (int)s2.charAt(i);
+		System.out.println(s2.charAt(i) + " : " + c);
+		sum2 += c;
+	}
+	System.out.println();
+	int abs = Math.abs(sum1 - sum2);
+	System.out.print("Sum of first string : " + sum1);
+	System.out.print("Sum of second string : " + sum2);
+	System.out.print("Absolute difference is : " + abs);
 
 }
 }
